92a8d00c109762f9a782a4f62f91c40bdcbad386
dehashizia <hiziadehas@gmail.com>
2024-09-21 12:59:04 +0200

    modifs accuil

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ function Menu() {
   return (
     <nav className="bg-blue-300 p-6">
       <Link href="/" className="mr-6 hover:bg-blue-600 hover:font-bold">
-        Acceuil
+        Accueil
       </Link>
       <Link href="/about" className="mr-6  hover:bg-blue-600 hover:font-bold">
         About Us
